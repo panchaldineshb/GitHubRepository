207317338ede3662db9e3abb623aa2c7dac5c0df
panchaldineshb <panchaldineshb@gmail.com>
2015-08-29 19:16:22 -0400

    Added get example, but still Phones.json file giving XHR error

--- CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/app/js/app.js
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/app/js/app.js
@@ -4,24 +4,29 @@
 
 var phonecatApp = angular.module('phonecatApp', [
   'ngRoute',
+  'ngResource',
   'phonecatAnimations',
   'phonecatControllers',
   'phonecatFilters',
   'phonecatServices'
 ]);
 
-phonecatApp.config(['$routeProvider',
-  function ($routeProvider) {
-      $routeProvider.
-        when('/phones', {
-            templateUrl: 'partials/phone-list.html',
-            controller: 'PhoneListController'
-        }).
-        when('/phones/:phoneId', {
-            templateUrl: 'partials/phone-detail.html',
-            controller: 'PhoneDetailController'
-        }).
-        otherwise({
-            redirectTo: '/phones'
-        });
-  }]);
+phonecatApp.config(['$routeProvider', '$locationProvider', '$httpProvider',
+   function ($routeProvider, $locationProvider, $httpProvider) {
+
+       $httpProvider.defaults.useXDomain = true;
+       delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+       $routeProvider.
+         when('/phones', {
+             templateUrl: 'partials/phone-list.html',
+             controller: 'PhoneListController'
+         }).
+         when('/phones/:phoneId', {
+             templateUrl: 'partials/phone-detail.html',
+             controller: 'PhoneDetailController'
+         }).
+         otherwise({
+             redirectTo: '/phones'
+         });
+   }]);
--- CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/app/js/services.js
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/app/js/services.js
@@ -4,9 +4,45 @@
 
 var phonecatServices = angular.module('phonecatServices', ['ngResource']);
 
-phonecatServices.factory('Phone', ['$resource',
-  function ($resource) {
+phonecatServices.factory('Phone', function ($http) {
+    var req = { method: 'GET', url: 'phones/:phoneId.json', params: { phoneId: 'phones' }, isArray: true };
+
+    var json = $http(req).then(function (response) {
+        return response.data;
+    });
+
+    var Phone = function (data) {
+        if (data) angular.copy(data, this);
+    };
+
+    Phone.query = function () {
+        return json.then(function (data) {
+            return data.map(function (project) {
+                return new Phone(project);
+            });
+        })
+    };
+
+    Phone.get = function (id) {
+        return json.then(function (data) {
+            var result = null;
+            angular.forEach(data, function (project) {
+                if (project.id == id) result = new Phone(project);
+            });
+            return result;
+        })
+    };
+
+    return Phone;
+});
+
+/*
+phonecatServices.factory('Phone', ['$resource', '$http',
+  function ($resource, $http) {
+      delete $http.defaults.headers.common['X-Requested-With'];
+
       return $resource('phones/:phoneId.json', {}, {
           query: { method: 'GET', params: { phoneId: 'phones' }, isArray: true }
       });
   }]);
+*/
