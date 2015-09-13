ca46a3dd43e467d40950f2171a6de7c7db453a0a
panchaldineshb <panchaldineshb@gmail.com>
2015-09-13 11:18:47 -0400

    DP:Calling /api/User call, but getting http 400 XHR error using  onject -- not working properly

--- CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/account/account.service.js
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/account/account.service.js
@@ -1,42 +1,48 @@
 ﻿'use strict';
 
-/* Services */
+/* Services
 
 angular.module('phonecatApp')
-.factory('Account', ['Base64', '$http', '$cookieStore', '$rootScope', '$timeout',
-    function (Base64, $http, $cookieStore, $rootScope, $timeout) {
+.factory('Contact', ['$resource',
+  function ($resource) {
+      return $resource('app/phones/:phoneId.json', {}, {
+          query: { method: 'GET', params: { phoneId: 'phones' }, isArray: true }
+      });
+  }])
+
+*/
+
+angular.module('phonecatApp')
+.factory('Account', ['Base64', '$http', '$resource', '$cookieStore', '$rootScope', '$timeout',
+    function (Base64, $http, $resource, $cookieStore, $rootScope, $timeout) {
     var service = {};
 
     var baseUrl = config.apiurl;
 
-    var url = baseUrl + 'api/User';
-
     service.Login = function (username, password, callback) {
         /* Dummy authentication for testing, uses $timeout to simulate api call
-             ----------------------------------------------*/
-        $timeout(function () {
-            var urlToMakeCall = url + '?Name=' + username;
-            $http({ method: 'GET', url: urlToMakeCall }).
-              success(function (data, status, headers, config) {
-                  // this callback will be called asynchronously
-                  // when the response is available
-                  debugger;
-              }).
-              error(function (data, status, headers, config) {
-                  // called asynchronously if an error occurs
-                  // or server returns response with an error status.
-                  debugger;
-              });
+           ----------------------------------------------*/
 
-            var response = { success: username === 'test' && password === 'test' };
+        // http://localhost:58045/api/User?Name=Shoya+Bali
+        var url = baseUrl + 'api/User?Name=:Name';
+
+        var User = $resource(url);
+
+        User.get({ Name: username }).$promise.then(function (data) {
+            // success
+            var response = { success: password === data.User.Pin };
             if (!response.success) {
                 response.message = 'Username or password is incorrect';
             }
             callback(response);
-        }, 1000);
+        }, function (errResponse) {
+            // fail
+            var response = { success: false, message: 'Username is invalid' };
+            callback(response);
+        });
 
         /* Use this for real authentication
-             ----------------------------------------------*/
+           ----------------------------------------------*/
         //$http.post('/api/authenticate', { username: username, password: password })
         //    .success(function (response) {
         //        callback(response);
