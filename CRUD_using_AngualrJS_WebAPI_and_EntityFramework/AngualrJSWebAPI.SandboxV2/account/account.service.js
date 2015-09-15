e128068ba72f0d6481b60909a778fa65051e73db
panchaldineshb <panchaldineshb@gmail.com>
2015-09-15 06:59:44 -0400

    DP:Still working on registration feature under organization

--- CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/account/account.service.js
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/account/account.service.js
@@ -53,31 +53,44 @@ angular.module('phonecatApp')
     return job;
 });
 
-*/
-
-/*
-Examples referred
-http://www.masnun.com/2013/08/28/rest-access-in-angularjs-using-ngresource.html
-http://devdactic.com/improving-rest-with-ngresource/
-
-*/
-
 // Authenticate Resource
 angular.module('phonecatApp')
 .factory('Authenticate', ['$resource',
-    function ($resourceut) {
+    function ($resource) {
         var serviceBaseUrl = config.apiurl;
-        var url = serviceBaseUrl + 'api/User?Name=:Name';
-
-        return $resource(
-           "'api/User?Name=:Name",
-           { Name: "@Name" },
-           {
-               "update": { method: "PUT" },
-               "reviews": { 'method': 'GET', 'params': { 'reviews_only': "true" }, isArray: true }
-           }
-       );
+        var url = serviceBaseUrl + 'api/User';
+        return $resource(url, {});
     }])
+
+
+
+*/
+
+/*
+
+Examples referred
+http://www.masnun.com/2013/08/28/rest-access-in-angularjs-using-ngresource.html
+http://devdactic.com/improving-rest-with-ngresource/
+http://www.4byte.cn/question/442009/angular-1-0-8-resource-with-multiple-optional-get-parameters.html
+
+
+If you need parameters to be mapped to the query parameters, not the path parameters, 
+you should not put the name of parameters into the path Eg student resource should look like this.:
+
+var  Student = $resource( '/app/student/' , 
+    {} 
+);
+Then invocation like this will work without any problems:
+
+Student . get ({ ID : 12 , courseId : 55 }); ? // Calls / app / student / ID = 12 & courseId = 55 
+Student . get ({ ID : 12 }); // Calls / app / student / ID? = 12 
+Student . get ({ courseId : 55 }); // Calls / app / student / courseId = 55?
+
+The others cases will work as well. Just do not confuse path parameters with query parameters.
+
+*/
+
+
 
 angular.module('phonecatApp')
 .factory('Account', ['Base64', '$http', '$resource', '$cookieStore', '$rootScope', '$timeout',
@@ -91,9 +104,10 @@ angular.module('phonecatApp')
            ----------------------------------------------*/
 
         // http://localhost:58045/api/User?Name=Shoya+Bali
-        var url = serviceBaseUrl + 'api/User?Name=:Name';
+        var url = serviceBaseUrl + 'api/User';
 
-        var User = $resource(url);
+        // we need parameters to be mapped to the query parameters, not the path parameters
+        var User = $resource(url, {});
 
         User.get({ Name: username }).$promise.then(function (data) {
             // success
--- CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/scripts/_references.js
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.SandboxV2/scripts/_references.js
@@ -1,18 +1,19 @@
-﻿/// <reference path="/app-services/animations.js" />
-/// <reference path="/app-services/app.js" />
+﻿/// <reference path="/app.js" />
+
+/// <reference path="/app-services/animations.js" />
 /// <reference path="/app-services/controllers.js" />
 /// <reference path="/app-services/directives.js" />
 /// <reference path="/app-services/filters.js" />
-/// <reference path="/app-services/.services.js" />
+/// <reference path="/app-services/services.js" />
 
 /// <reference path="/about/about.controller.js" />
-/// <reference path="/about/about.services.js" />
+/// <reference path="/about/about.service.js" />
 
 /// <reference path="/home/home.controller.js" />
-/// <reference path="/home/home.services.js" />
+/// <reference path="/home/home.service.js" />
 
 /// <reference path="/contact/contact.controller.js" />
-/// <reference path="/contact/contact.services.js" />
+/// <reference path="/contact/contact.service.js" />
 
 /// <reference path="/account/account.service.js" />
 /// <reference path="/account/account.controller.js" />
