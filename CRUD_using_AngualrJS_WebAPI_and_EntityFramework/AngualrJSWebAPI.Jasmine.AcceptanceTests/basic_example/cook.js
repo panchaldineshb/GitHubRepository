1de0d6136e414b4611be2edf02c22e6f12122bf1
panchaldineshb <panchaldineshb@gmail.com>
2015-10-04 19:40:51 -0400

    DP:Jasmine Acceptance Tests added -- referred book AngularJS Testing Cookbook

---
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.Jasmine.AcceptanceTests/basic_example/cook.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+/* Controllers */
+
+angular
+    .module('cookbook', [])
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.emcee = 'Kool G Rap';
+    }])
+
+
+expect($scope.emcee).toEqual('Kool G Rap');
---
+++ CRUD_using_AngualrJS_WebAPI_and_EntityFramework/AngualrJSWebAPI.Jasmine.AcceptanceTests/tests/cookbookSpec.js
@@ -0,0 +1,11 @@
+﻿describe('MainCtrl', function () {
+    beforeEach(module('cookbook'));
+});
+
+it('should assign the correct rapper to scope',
+inject(function ($controller, $rootScope) {
+    var $scope = $rootScope.$new();
+    $controller('MainCtrl', {
+        $scope: $scope
+    });
+}));
